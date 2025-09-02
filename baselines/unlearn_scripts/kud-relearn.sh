21d2e825509cb9899d925b38b047d95d9b67b161
ShuHan <1633203678@qq.com>
2025-09-02 21:59:10 +0800

    updated

--- baselines/unlearn_scripts/kud-relearn.sh
+++ baselines/unlearn_scripts/kud-relearn.sh
@@ -19,8 +19,27 @@ ds_config="../config/ds_z0_config.json"
 loss_types=("relearn_klr_gdr")
 max_length=512
 
+echo "model_family: ${model_family}"
+echo "model_path: ${model_path}"
+echo "lr: ${lr}"
+echo "num_epochs: ${num_epochs}"
+echo "ds_config: ${ds_config}"
+echo "loss_types: ${loss_types}"
+echo "max_length: ${max_length}"
+
+echo "forget_data_path: ${forget_data_path}"
+echo "retain_data_path: ${retain_data_path}"
+echo "idonknow_file_path: ${idonknow_file_path}"
+
+mkdir -p ../../logs/relearn
+
+# Extract model name from save_dir and add date for log file naming
+current_date=$(date +%Y%m%d_%H%M%S)
+log_file="../../logs/relearn/${model_family}_lora_privacy_${current_date}.log"
+
+
 for loss_type in "${loss_types[@]}"; do
     echo $loss_type
     save_dir="../../memory/${model_family}_${loss_type}_${data_subset}_${max_length}_${lr}"
-    CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=$master_port ../unlearn.py --config-name=forget_lora.yaml batch_size=1 gradient_accumulation_steps=4 model_family=${model_family} lr=${lr} model_path=${model_path} forget_data_path=${forget_data_path} retain_data_path=${retain_data_path} idonknow_file_path=${idonknow_file_path} loss_type=${loss_type} ds_config=${ds_config} max_length=${max_length} save_dir=${save_dir} num_epochs=${num_epochs}
+    CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=$master_port ../unlearn.py --config-name=forget_lora.yaml batch_size=1 gradient_accumulation_steps=4 model_family=${model_family} lr=${lr} model_path=${model_path} forget_data_path=${forget_data_path} retain_data_path=${retain_data_path} idonknow_file_path=${idonknow_file_path} loss_type=${loss_type} ds_config=${ds_config} max_length=${max_length} save_dir=${save_dir} num_epochs=${num_epochs}  2>&1 | tee "${log_file}"
 done
